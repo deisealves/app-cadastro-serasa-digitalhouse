3fc805d510c0b6da28c2765c22bdfbbf71242347
deisealves <102121940+deisealves@users.noreply.github.com>
2022-08-17 02:27:03 -0300

    adicionando app cadastro

---
+++ src/main/resources/db/migration/V2__Populate_Table_Pessoas.sql
@@ -0,0 +1,8 @@
+INSERT INTO `tb_pessoas` VALUES 
+(1,'deise@gmail.com.com','deise','deyse','soares'),
+(2,'filhosalmeidaalexandre@gmail.com','Julia','Juju','Almeida'),
+(3,'davi@teste.com','Davi','Davizinho','Almeida'),
+(4,'jaugusto@testecom','João Augusto','João','Almeida'),
+(5,'tereza@teste.com','Tereza','Tereza','Almeida'),
+(6,'fabiane@gmail.com','Fabiane','Fabi','Almeida');
+
